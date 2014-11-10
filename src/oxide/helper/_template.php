337d7fcf23636e46816ea2ef3a7d352ea8ea5d6f
alamin3000 <al.ahmed@outlook.com>
2014-11-10 10:21:13 -0500

    added scripts and snippet.

--- src/oxide/helper/_template.php
+++ src/oxide/helper/_template.php
@@ -42,7 +42,7 @@ abstract class _template {
       }
       
       if($identifier) {
-         self::$snippets[$identifier] = $identifier;
+         self::$snippets[$identifier][] = $identifier;
       } else {
          self::$snippets[] = $code;
       }
