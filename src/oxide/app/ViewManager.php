06ae9645c1214b9a72c673083ba9d46c9c147cea
alamin3000 <al.ahmed@outlook.com>
2015-01-04 23:44:52 -0500

    helpers.

--- src/oxide/app/ViewManager.php
+++ src/oxide/app/ViewManager.php
@@ -248,8 +248,9 @@ class ViewManager {
          $layoutView = $this->getLayoutView();
          $renderingView = $layoutView;
          $layoutView->addPartial($view, $layoutView->contentKey);
-         if($view->getRenderer() instanceof Page) {
-            $view->setParent($layoutView);
+         $renderer = $view->getRenderer();
+         if($renderer instanceof Page) {
+            $renderer->setParent($layoutView);
          }
       }
             
