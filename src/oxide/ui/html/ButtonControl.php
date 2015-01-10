c0ec92a48ccec82afd30eb34d44d971333e32cf7
alamin3000 <al.ahmed@outlook.com>
2015-01-10 12:42:17 -0500

    ui complements refactoring, fixing.

--- src/oxide/ui/html/ButtonControl.php
+++ src/oxide/ui/html/ButtonControl.php
@@ -13,17 +13,12 @@ class ButtonControl extends Control {
 	 * construct the button control
 	 * 
 	 * @param string $name
-	 * @param string $value
 	 * @param string $label
 	 * @param array $attrbs
 	 */
-	public function __construct($type, $name, $label = null,  $attrbs = null) {
-		parent::__construct('button', $name, null, $label,  $attrbs);
+	public function __construct($type, $name, $html,  $attrbs = null) {
+		parent::__construct('button', $name, null, null,  $attrbs);
       $this->type = $type;
+      $this->setHtml($html);
 	}
-   
-   public function setLabel($value) {
-      parent::setLabel($value);
-      $this->setHtml($value);
-   }
 }
