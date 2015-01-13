adf016e5429b14a33d8e60cdcc5ce2a28861cacd
alamin3000 <al.ahmed@outlook.com>
2015-01-13 12:11:51 -0500

    master helper and refactoring

--- src/oxide/util/ArrayString.php
+++ src/oxide/util/ArrayString.php
@@ -20,7 +20,21 @@ class ArrayString
          $callback = $this->_stringify_callback;
          return $callback($this);
       } else {
-         return implode('', $this->_t_array_storage);
+         return self::toString($this->_t_array_storage);
       }
+   }
+   
+   public static function toString($arg) {
+      if(is_scalar($arg)) return $arg;
+      else if($arg instanceof \oxide\ui\Renderer) return $arg->render();
+      else if($arg instanceof Stringify) return $arg->__toString();
+      else if(is_array ($arg) || $arg instanceof \Iterator) {
+         $buf = '';
+         foreach($arg as $val) {
+            $buf .= self::toString($val);
+         }
+         return $buf;
+      }
+      else return (string) $arg;
    }
 }
