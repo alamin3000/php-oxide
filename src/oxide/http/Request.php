582dd716bb5426fda95fafffafca1f4f3008bf16
alamin3000 <al.ahmed@outlook.com>
2015-01-16 18:14:33 -0500

    issues/fixes/refactor

--- src/oxide/http/Request.php
+++ src/oxide/http/Request.php
@@ -72,7 +72,6 @@ class Request {
    public static function createFromUrl($url) {
       $request = new self();
       $uris = parse_url($url);
-      var_dump($uris);
       $request->_url = $url;
       $request->_uriComponents = $uris;
       
