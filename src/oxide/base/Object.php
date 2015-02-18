44b2538c3152eafe58213392bd8e78430defe689
alamin3000 <al.ahmed@outlook.com>
2015-02-18 11:17:08 -0500

    bug fix

--- src/oxide/base/Object.php
+++ src/oxide/base/Object.php
@@ -38,7 +38,6 @@ class Object
    
    /**
     * Check if given string is serialized
-    * @credit WordPress
     * @access public
     * @static
     * @param mixed $str
@@ -46,28 +45,7 @@ class Object
     */
    public static function serialized($data) {
 	   // if it isn't a string, it isn't serialized
-		if (!is_string( $data ) )
-		  return false;
-		$data = trim( $data );
-		if ( 'N;' == $data )
-		  return true;
-		if ( !preg_match( '/^([adObis]):/', $data, $badions ) )
-		  return false;
-		switch ( $badions[1] ) {
-		  case 'a' :
-		  case 'O' :
-		  case 's' :
-		      if ( preg_match( "/^{$badions[1]}:[0-9]+:.*[;}]\$/s", $data ) )
-		          return true;
-		      break;
-		  case 'b' :
-		  case 'i' :
-		  case 'd' :
-		      if ( preg_match( "/^{$badions[1]}:[0-9.E-]+;\$/", $data ) )
-		          return true;
-		      break;
-		}
-		return false;
+		return (@unserialize($string) !== false || $string == 'b:0;');
    }
    
    
