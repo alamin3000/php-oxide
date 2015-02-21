5e7a29539749a81b19b0170c9530b0207a45de23
alamin3000 <al.ahmed@outlook.com>
2015-02-20 20:17:46 -0500

    testing

--- src/oxide/ui/html/Element.php
+++ src/oxide/ui/html/Element.php
@@ -132,6 +132,7 @@ class Element
          // now perform the wrappers
          if(!empty($this->wrappers)) {
             reset($this->wrappers);
+            print 'here';
             foreach($this->wrappers as $wrapper) {
                $buffer->prepend($wrapper->renderOpen());
                $buffer->append($wrapper->renderClose());
